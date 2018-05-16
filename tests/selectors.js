55fe4e3fb22332af5258c3cd0a67b889c2dd3cb3
thangemarketing <39334433+thangemarketing@users.noreply.github.com>
2018-05-16 18:11:47 +0700

    Add files via upload

---
+++ tests/selectors.js
@@ -0,0 +1,73 @@
+/* eslint-env mocha */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const expect = require('chai').expect;
+const uncss = require('uncss');
+
+const readFile = file => fs.readFileSync(path.join(__dirname, file), 'utf-8');
+
+let rawcss = readFile('output.css');
+const urlcss = readFile('outputUrl.css');
+const tests = fs.readdirSync(path.join(__dirname, 'fixtures/'));
+
+/* Only read through CSS files */
+tests.forEach(test => {
+    if (test.includes('.css')) {
+        readFile(`fixtures/${test}`);
+    }
+});
+
+/*
+Tests without grunt-uncss
+*/
+describe('uncss', () => {
+    /* Wait until uncss finished doing its thing before running our tests */
+    before(done => {
+        /* New api from issue #44 */
+        uncss(readFile('index.html'), {
+            csspath: 'tests'
+        }, (err, output) => {
+            if (err) {
+                throw err;
+            }
+            rawcss = output;
+            done();
+        });
+    });
+
+    it('should output something', () => {
+        expect(rawcss).not.to.equal(false);
+        expect(urlcss).not.to.equal(false);
+    });
+
+    it('should not be an empty string', () => {
+        expect(rawcss).to.have.length.above(0);
+    });
+
+    it('should read .uncssrc files', done => {
+        uncss(readFile('index.html'), {
+            csspath: 'tests',
+            report: true,
+            uncssrc: path.normalize('tests/.uncssrc')
+        }, (err, res, report) => {    // eslint-disable-line consistent-return
+            if (err) {
+                return done(err);
+            }
+            expect(err).to.equal(null);
+            expect(res).to.equal(rawcss);
+            expect(report.original).not.to.equal(null);
+            done();
+        });
+    });
+
+    /* We're testing that the CSS is stripped out from the result,
+       not that the result contains the CSS in the unused folder. */
+    tests.forEach(test => {
+        it(`should handle ${test.split('.')[0]}`, () => {
+            expect(rawcss).to.not.include.string(readFile(`unused/${test}`));
+        });
+    });
+});
